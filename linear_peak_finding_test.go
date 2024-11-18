770adecc54662ed9d7f0589c21fe22a781303e59
Rupert Hammond <ruperthammond@costellomedical.com>
2024-11-18 16:02:43 +0000

    forgot to add the linear peak file test

---
+++ linear_peak_finding_test.go
@@ -0,0 +1,51 @@
+package peakfinder
+
+import "testing"
+
+func TestLogNPeakFinderSmallCase(t *testing.T) {
+	input := []int{1, 2, 3, 4, 6, 5}
+	finder := &LinearPeakFinder{numbers: input}
+	want := 6
+	peak, err := finder.FindPeak(input)
+	if err != nil {
+		t.Fatal(err.Error())
+	} else if peak != want {
+		t.Fatalf("Failed to find peak correctly, wanted: %v, got: %v", want, peak)
+	}
+}
+
+func TestLogNPeakFinderSingleValue(t *testing.T) {
+	input := []int{1}
+	finder := &LinearPeakFinder{numbers: input}
+	want := 1
+	peak, err := finder.FindPeak(input)
+	if err != nil {
+		t.Fatal(err.Error())
+	} else if peak != want {
+		t.Fatalf("Failed to find peak correctly, wanted: %v, got: %v", want, peak)
+	}
+}
+
+func TestLogNPeakFinderAllValuesAreTheSame(t *testing.T) {
+	input := []int{0, 0, 0, 0, 0}
+	finder := &LinearPeakFinder{numbers: input}
+	want := 0
+	peak, err := finder.FindPeak(input)
+	if err != nil {
+		t.Fatal(err.Error())
+	} else if peak != want {
+		t.Fatalf("Failed to find peak correctly, wanted: %v, got: %v", want, peak)
+	}
+}
+
+func TestEdgePeaksAreCorrectlyHandled(t *testing.T) {
+	input := []int{1, 0}
+	finder := &LinearPeakFinder{numbers: input}
+	want := 1
+	peak, err := finder.FindPeak(input)
+	if err != nil {
+		t.Fatal(err.Error())
+	} else if peak != want {
+		t.Fatalf("Failed to find peak correctly, wanted: %v, got: %v", want, peak)
+	}
+}
