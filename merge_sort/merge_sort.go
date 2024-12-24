ca87d3b9283f0e0dcbd0adbeb7c9db015dbf5bf7
Rupert Hammond <ruperthammond@costellomedical.com>
2024-12-24 06:51:19 +0000

    initial crack at merge sort, stack overflow errors as predicted lol

--- merge_sort/merge_sort.go
+++ merge_sort/merge_sort.go
@@ -1,9 +1,43 @@
 package mergesort
 
 /*
-Perform merge sort on a slice of intergers
+Perform acending order merge sort on a slice of intergers
 */
 func PerformMergeSort(numbers []int) []int {
+	n := len(numbers)
+	sorted_list := make([]int, 0, n)
 
-	return numbers
+	sorted_list = append(sorted_list, merge_arr(PerformMergeSort(numbers[:n/2]), PerformMergeSort(numbers[n/2:n]))...)
+
+	return sorted_list
+}
+
+/*
+Take the contents of two equal sized lists and merge them in ascending order, two finger style
+*/
+func merge_arr(a, b []int) []int {
+	if len(a) == 1 && len(b) == 1 {
+		if a[0] > b[0] {
+			return []int{b[0], a[0]}
+		} else {
+
+			return []int{a[0], b[0]}
+		}
+	}
+
+	sorted_array := make([]int, 0, len(a)+len(b))
+	p1 := 0
+	p2 := 0
+
+	for i := 0; i < len(a)+len(b); i++ {
+		if a[p1] > b[p2] {
+			sorted_array = append(sorted_array, b[p2])
+			p2 += 1
+		} else {
+			sorted_array = append(sorted_array, a[p1])
+			p1 += 1
+		}
+	}
+
+	return sorted_array
 }
